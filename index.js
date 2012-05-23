00372bc0d1d92907e679d8e81e411ade2755e869
Raynos <raynos2@gmail.com>
2012-05-23 14:45:42 +0100

    first

---
+++ index.js
@@ -0,0 +1,110 @@
+var extend = require("xtend"),
+    EventEmitter = require("events").EventEmitter.prototype,
+    test = makeTestAndRun(null),
+    currentEmitter
+
+test.makeTest = makeTestAndRun
+
+process.on("uncaughtException", reportError)
+
+module.exports = test
+
+function makeTestAndRun(parent) {
+    var test = extend(makeTest(parent), EventEmitter)
+
+    process.nextTick(run)
+
+    return test
+
+    function run() {
+        var node = findNode(test)
+        if (node) {
+            currentEmitter = test
+            currentEmitter.errorName = node.testName
+            runTest({
+                emitter: test, 
+                test: node, 
+                name: node.testName, 
+                block: node.block, 
+                callback: next
+            })
+        } else {
+            test.emit("end")
+        }
+
+        function next() {
+            node.block = null
+            node.testName = null
+            run()
+        }
+    }
+}
+
+function makeTest(parent) {
+    extend(test, {
+        nodes: [],
+        parent: parent
+    })
+
+    return test
+
+    function test(name, block) {
+        test.nodes.push(extend(makeTest(test), {
+            testName: name,
+            block: block
+        }))
+    }    
+}
+
+function findNode(tree) {
+    var nodes = tree.nodes
+    for (var i = 0; i < nodes.length; i++) {
+        var node = nodes[i]
+        if (node.testName) {
+            return node
+        }
+        node = findNode(node)
+        if (node !== null && node.testName) {
+            return node
+        }
+    }
+    return null
+}
+
+function runTest(options) {
+    var emitter = options.emitter,
+        block = options.block,
+        test = options.test,
+        name = options.name
+
+    emitter.emit("test", name)
+
+    try {
+        if (block.length === 2) {
+            block(test, done)
+        } else {
+            block(test)
+            done()
+        }
+    } catch (err) {
+        done(err)
+    }
+
+    function done(err) {
+        if (err) {
+            emitter.emit("error", err, name)
+        } else {
+            emitter.emit("pass", name)
+        }
+        emitter.emit("test end", name)
+        options.callback()
+    }
+}
+
+function reportError(err) {
+    if (currentEmitter && currentEmitter._events.error) {
+        currentEmitter.emit("error", err, currentEmitter.errorName)
+    } else {
+        console.error("ERROR in test", err.message)
+    }
+}
--- test/suitestack.js
+++ test/suitestack.js
@@ -1,1 +1,159 @@
-var test = require("suitestack")
+var test = require("suitestack"),
+    assert = require("assert"),
+    total = 0
+
+// report
+test.on("error", function (err) {
+    console.error("FAIL:", err.message, err.stack)
+})
+
+test.on("test", function () {
+    total++
+})
+
+test.on("test end", function () {
+    total--
+})
+
+test.on("pass", function (name) {
+    console.log("PASS:", name)
+})
+
+test.on("end", function () {
+    assert(total === 0, "tests did not finish")
+    console.log("test finished")
+})
+
+// tests
+test("test is a function", function () {
+    assert(typeof test === "function", "test is not a function")
+})
+
+test("test is an eventemitter", function () {
+    assert(test.on, "test is not an eventemitter")
+})
+
+test("test fires an error event when test block fails", function (_, done) {
+    var t = test.makeTest()
+
+    t.on("error", function (err, name) {
+        assert(err.message === "fail", "test did not fail")
+        assertName(name)
+        done()
+    })
+
+    t("name", function () {
+        assert(false, "fail")
+    })
+})
+
+test("test works asynchronously", function (_, done) {
+    var t = test.makeTest()
+
+    t.on("pass", function (name) {
+        assertName(name)
+        done()
+    })
+
+    t("name", function (_, done) {
+        setTimeout(function () {
+            done()
+        }, 50)
+    })
+})
+
+test("test fires a test event when you start a test", function (_, done) {
+    var t = test.makeTest()
+
+    t.on("test", function (name) {
+        assertName(name)
+        done()
+    })
+
+    t("name", function () {})
+})
+
+test("test fires a test end event when a test ends", function (_, done) {
+    var t = test.makeTest(),
+        count = 0
+
+    t.on("test end", function (name) {
+        assertName(name)
+        assert(count === 1, "count is incorrect")
+        done()
+    })
+
+    t("name", function () {
+        count++
+    })
+})
+
+test("tests fires pass when a tests passes", function (_, done) {
+    var t = test.makeTest(),
+        count = 0
+
+    t.on("pass", function (name) {
+        assertName(name)
+        assert(count === 1, "count is incorrect")
+        done()
+    })
+
+    t("name", function () {
+        count++
+    })
+})
+
+test("asynchronous exceptions are caught", function (_, done) {
+    var t = test.makeTest()
+
+    t.on("error", function (err, name) {
+        assertName(name)
+        assert(err.message === "fail", "error did not fail")
+        done()
+    })
+
+    t("name", function (_, done) {
+        setTimeout(function () {
+            assert(false, "fail")
+        }, 50)
+    })
+})
+
+test("tests run in test order", function (_, done) {
+    var t = test.makeTest(),
+        count = 0
+
+    t.on("end", function () {
+        assert(count === 7, "not all tests finished")
+        done()
+    })
+
+    t("first", function (t) {
+        assert(++count === 1, "count is out of order")
+        t("second", function () {
+            assert(++count === 2, "count is out of order")
+        })
+
+        t("third", function () {
+            assert(++count === 3, "count is out of order")
+        })
+    })
+
+    t("fourth", function (t) {
+        assert(++count === 4, "count is out of order")
+        t("fifth", function (t) {
+            assert(++count === 5, "count is out of order")
+            t("sixth", function () {
+                assert(++count === 6, "count is out of order")
+            })
+        })
+
+        t("seventh", function () {
+            assert(++count === 7, "count is out of order")
+        })
+    })
+})
+
+function assertName(name) {
+    assert(name === "name", "test name is incorrect")
+}
